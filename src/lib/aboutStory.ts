f92f87b23b9cddbc9f29553e403ff5b91ea2a441
dannypellatt <108154659+dannypellatt@users.noreply.github.com>
2026-09-14 14:40:17 -0700

    Update aboutStory.ts

--- src/lib/aboutStory.ts
+++ src/lib/aboutStory.ts
@@ -41,7 +41,7 @@ export const storySlides: StorySlide[] = [
   {
     index: 6,
     title: "Okay, cool I guess. Now show me the receipts.",
-    body: "Well… That’s the story so far! I'm currently living life in Carlsbad, CA, with my dog Banjo.\n\nIf you want to see how that experience translates into product work, take a look at a few of the projects I’ve led:",
+    body: "Well… That’s the story so far! Nowadays you'll find me living life in Carlsbad, CA, with my dog Banjo.\n\nIf you want to see how that experience translates into product work, take a look at a few of the projects I’ve led:",
     photoSrc: "/images/MeAndBanjo.jpeg",
     isFinal: true,
   },
